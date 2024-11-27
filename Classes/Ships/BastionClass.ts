7a0db2acd9405ed457299df8fbd9945d1d63f804
Taylor Simpson <tss72@byu.edu>
2024-11-27 14:55:59 -0700

    feat: create Bastion class

---
+++ Classes/Ships/BastionClass.ts
@@ -0,0 +1,28 @@
+import Ship from "../ShipClass"
+
+class Bastion extends Ship {
+    constructor(direction: string, x_coord: number, y_coord: number, player: number) {
+        super('Bastion', '../../Assets/Bastion1.svg', 2, 2, 1, 3, 1, 1, [0, 0, 0, 0], 2, 2, direction, x_coord, y_coord, 3, player)
+    }
+
+    defendShip(ship: Ship) {
+        // Check that target ship is friendly
+        if (ship.getPlayer() == this.getPlayer()) {
+            const current_bonus: number[] = ship.getBonus()
+            let new_bonus: number[] = current_bonus
+            // Increment the defense portion of the target ship's bonus
+            new_bonus[1] += 1
+
+            // Check if the target ship is adjacent
+            const dist: number = this.findDistance(ship.getX(), ship.getY())
+            if (dist <= 1) {
+                // Increment the defense portion of the target ship's bonus further
+                new_bonus[1] += 1
+            }
+
+            ship.setBonus(new_bonus)
+        }
+    }
+}
+
+export default Bastion
