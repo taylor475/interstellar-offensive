3837b4458015f05c028c0ba8264fbba1d8634670
Taylor Simpson <tss72@byu.edu>
2024-11-27 11:53:40 -0700

    feat: add bonus attribute to Fighter class

--- Classes/Ships/FighterClass.ts
+++ Classes/Ships/FighterClass.ts
@@ -2,7 +2,7 @@ import Ship from "../ShipClass"
 
 class Fighter extends Ship {
     constructor(direction: string, x_coord: number, y_coord: number, player: number) {
-        super('Fighter', 1, 1, 1, 0, 1, 4, 1, 1, direction, x_coord, y_coord, 1, player)
+        super('Fighter', 1, 1, 1, 0, 1, 4, [0, 0, 0, 0], 1, 1, direction, x_coord, y_coord, 1, player)
     }
 }
 
