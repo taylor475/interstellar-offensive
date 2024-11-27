f2d4d64626dfbc94169ad219912c31519f2beb01
Taylor Simpson <tss72@byu.edu>
2024-11-27 14:18:39 -0700

    fix: correct cruiser atk value

--- Classes/Ships/CruiserClass.ts
+++ Classes/Ships/CruiserClass.ts
@@ -2,7 +2,7 @@ import Ship from "../ShipClass"
 
 class Cruiser extends Ship {
     constructor(direction: string, x_coord: number, y_coord: number, player: number) {
-        super('Cruiser', '../../Assets/Cruiser1.svg', 1, 2, 1, 1, 1, 3, [0, 0, 0, 0], 2, 3, direction, x_coord, y_coord, 1, player)
+        super('Cruiser', '../../Assets/Cruiser1.svg', 1, 2, 2, 1, 1, 3, [0, 0, 0, 0], 2, 3, direction, x_coord, y_coord, 1, player)
     }
 }
 
