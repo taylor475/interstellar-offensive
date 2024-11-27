77775d9375c5103a09ed7409fadf81e01ab065fe
Taylor Simpson <tss72@byu.edu>
2024-11-27 13:08:22 -0700

    feat: create Destroyer class

---
+++ Classes/Ships/DestroyerClass.ts
@@ -0,0 +1,9 @@
+import Ship from "../ShipClass"
+
+class Destroyer extends Ship {
+    constructor(direction: string, x_coord: number, y_coord: number, player: number) {
+        super('Destroyer', 1, 1, 2, 0, 1, 3, [0, 0, 0, 0], 1, 1, direction, x_coord, y_coord, 1, player)
+    }
+}
+
+export default Destroyer
