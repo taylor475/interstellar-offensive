725c6283f8242383ac3388ee76ac18f83adf1929
Taylor Simpson <tss72@byu.edu>
2024-11-27 11:14:52 -0700

    refactor: convert ShipClass to typescript

--- Classes/ShipClass.ts
+++ Classes/ShipClass.ts
@@ -1,31 +1,30 @@
 import Object from "./ObjectClass"
 
 class Ship extends Object {
-    #atk
-    #def
-    #range
-    #cost
-    #play
+    private atk: number
+    private def: number
+    private range: number
+    private cost: number
+    private play: number
     
-    constructor(type, length, width, attack, defense, range, movement, health, max_health, direction, x_coord, y_coord, cost, player) {
+    constructor(type: string, length: number, width: number, attack: number, defense: number, range: number, movement: number, health: number, max_health: number, direction: string, x_coord: number, y_coord: number, cost: number, player: number) {
         super(type, length, width, movement, health, max_health, direction, x_coord, y_coord)
-        this.#atk = attack
-        this.#def = defense
-        this.#range = range
-        this.#cost = cost
-        this.#play = player
+        this.atk = attack
+        this.def = defense
+        this.range = range
+        this.cost = cost
+        this.play = player
     }
 
     // Moves the ship to a given location
-    moveShip(point_x, point_y) {
+    moveShip(point_x: number, point_y: number) {
         // Find distance between ship and destination
-        let dist = this.findDistance(point_x, point_y)
+        const dist: number = this.findDistance(point_x, point_y)
 
         // Move ship if the ship has adequate speed
-        // TODO Figure out how speed is determined (ship-by-ship basis or number of player actions)
-        if (dist <= speed) {
-            let x_diff = this.getX() - point_x
-            let y_diff = this.getY() - point_y
+        if (dist <= this.getMovement()) {
+            const x_diff: number = this.getX() - point_x
+            const y_diff: number = this.getY() - point_y
 
             if (abs(x_diff) >= abs(y_diff)) {
                 if (x_diff > 0) {
@@ -50,84 +49,84 @@ class Ship extends Object {
     }
 
     // Attacks enemy ship
-    attackShip(enemy) {
-        if (enemy.getPlayer() != this.#play) {
+    attackShip(enemy: Ship) {
+        if (enemy.getPlayer() != this.play) {
             // Find distance between ship and enemy
-            let dist = this.findDistance(enemy.getX(), enemy.getY())
+            const dist: number = this.findDistance(enemy.getX(), enemy.getY())
 
             // Begin attack sequence if enemy is in range
             if (dist <= this.range) {
-                if (this.#atk > enemy.getDefense()) {
-                    let damage = enemy.getDefense() - this.#atk
+                if (this.atk > enemy.getDefense()) {
+                    const damage: number = enemy.getDefense() - this.atk
                     enemy.takeDamage(damage)
                 }
             }
         }
     }
 
-    takeDamage(damage) {
-        let current_health = this.getHealth()
+    takeDamage(damage: number) {
+        const current_health: number = this.getHealth()
 
         this.setHealth(current_health - damage)
         // TODO: Add logic for a dead ship
     }
 
     // Finds the distance between the ship's location and a given point
-    findDistance(pt_x, pt_y) {
-        let ship_x = this.getX()
-        let ship_y = this.getY()
+    findDistance(pt_x: number, pt_y: number) {
+        const ship_x: number = this.getX()
+        const ship_y: number = this.getY()
 
-        let dist_x = Math.abs(ship_x - pt_x)
-        let dist_y = Math.abs(ship_y - pt_y)
+        const dist_x: number = Math.abs(ship_x - pt_x)
+        const dist_y: number = Math.abs(ship_y - pt_y)
 
-        let tot_dist = Math.sqrt(dist_x ^ 2 + dist_y ^ 2)
+        const tot_dist: number = Math.sqrt(dist_x ^ 2 + dist_y ^ 2)
 
         return tot_dist
     }
 
     // Getter method for the atk attribute
     getAttack() {
-        return this.#atk
+        return this.atk
     }
 
     // Setter method for the atk attribute
-    setAttack(attack) {
-        this.#atk = attack
+    setAttack(attack: number) {
+        this.atk = attack
     }
 
     // Getter method for the def attribute
     getDefense() {
-        return this.#def
+        return this.def
     }
 
     // Setter method for the def attribute
-    setDefense(defense) {
-        this.#def = defense
+    setDefense(defense: number) {
+        this.def = defense
     }
 
     // Getter method for the range attribute
     getRange() {
-        return this.#range
+        return this.range
     }
 
     // Setter method for the range attribute
-    setRange(range) {
-        this.#range = range
+    setRange(range: number) {
+        this.range = range
     }
 
     // Getter method for the cost attribute
     getCost() {
-        return this.#cost
+        return this.cost
     }
 
     // Getter method for the play attribute
     getPlayer() {
-        return this.#play
+        return this.play
     }
 
     // Setter method for the play attribute
-    setPlayer(player) {
-        this.#play = player
+    setPlayer(player: number) {
+        this.play = player
     }
 }
 
